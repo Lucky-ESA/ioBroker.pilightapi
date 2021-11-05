c7410f0a9655f59f23c13c6c2acc97335e63e543
Lucky-ESA <83980942+Lucky-ESA@users.noreply.github.com>
2021-11-05 17:58:48 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const utils = require('@iobroker/adapter-core');
 const net = require("net");
 const Json2iob = require("./lib/json2iob");
 
-class PilightAPI extends utils.Adapter {
+class Pilightapi extends utils.Adapter {
 
     /**
      * @param {Partial<utils.AdapterOptions>} [options={}]
@@ -22,8 +22,6 @@ class PilightAPI extends utils.Adapter {
         });
         this.on('ready', this.onReady.bind(this));
         this.on('stateChange', this.onStateChange.bind(this));
-        // this.on('objectChange', this.onObjectChange.bind(this));
-        // this.on('message', this.onMessage.bind(this));
         this.on('unload', this.onUnload.bind(this));
     }
 
@@ -32,165 +30,185 @@ class PilightAPI extends utils.Adapter {
      */
     async onReady() {
         // Initialize your adapter here
-        this.conneted = this.connectTopi();
-
-this.log.info('conneted: ' + this.conneted);
-
-        await this.connectToWS();
+        this.setState("info.connection", false, true);
+        this.config = {
+            pilightip: this.config.pilightip || 'localhost',
+            pilightdaemonport: this.config.pilightdaemonport || 5000
+        };
+        this.jsonString = "";
+        this.countS = 0;
         this.json2iob = new Json2iob(this);
-
         this.subscribeStates('*');
-
+        await this.connectToNet();
     }
 
-    async connectTopi() {
-            this.check = net.createConnection({ host: this.config.pilightip, port: this.config.pilightdaemonport }, () => {
-            this.check.write('{"action": "identify"}\n');
-        });
-        await this.checkip = false;
-        this.check.on('data', (data) => {
-            try {
-	         if (typeof data === "object") {
-this.log.info("Object OK");
-                    const jsondata = JSON.parse(data.toString("utf-8"));
-	             if (typeof jsondata.status !== "undefined") {
-this.log.info("Type OK");
-	                 if (jsondata.status === "success") {
-this.log.info("True OK");
-	                     await this.checkip = true;
-this.log.info("True OK: " + this.checkip);
-                        }
-	             }
-	         }
-            } catch (err) {
-                this.log.error(err);
-            }
-
-            this.check.end();
-        });
-
-        this.check.on('end', () => {
-            this.log.info('disconnected from server');
-        });
-
-        this.check.on('error', (err) => {
-            throw err;
-        });
-this.log.info("True NOK: " + this.checkip);
-        return await this.checkip;
-    }
-
-    async connectToWS() {
+    async connectToNet() {
+        if (this.client) {
+            this.client.end();
+        }
+        this.countS = 0
         this.client = net.createConnection({ host: this.config.pilightip, port: this.config.pilightdaemonport }, () => {
             this.client.write('{"action": "identify","options":{"core": 1,"receiver": 1,"config": 1,"forward": 1},"uuid": "0000-d0-63-00-000000","media": "all"}\n');
-            this.client.write('{"action": "request config"}\n');
         });
 
-        this.client.on('data', (data) => {
+        this.client.on('data', async (data) => {
+            if (this.wsHeartbeatTimeout) {
+                clearTimeout(this.wsHeartbeatTimeout);
+            }
+            this.wsHeartbeatTimeout = setTimeout(() => {
+                this.log.info("Lost Socket connection. Reconnect Socket");
+                this.client.end();
+                setTimeout(() => {
+                    this.connectToNet();
+                }, 2000);
+            }, 60 * 1000);
             try {
-	         if (typeof data === "object") {
-	             if (data.toString().includes('success', 'failure')) {
-
-//this.log.info("Testen1: " + data.toString("utf-8"));
-
-	                 const jsonMessage = data.toString("utf-8").replace(/\[/g, '').replace(/\]/g, '').replace(/\n/g, '');
-	                 const loginval = JSON.parse(jsonMessage.split("}{")[0] + "}");
-	                 const configval = JSON.parse("{" + jsonMessage.split("}{")[1]);
-                        if(loginval.status === "success") {
-                            this.log.info("Server connected!");
-
-
-//this.log.info("Testen2: " + JSON.stringify(configval["config"]["devices"]));
-//this.log.info("Testen3: " + JSON.stringify(configval["config"]["gui"]));
-
-
-//const ha1 = {"Lamp1":{"protocol":"elro_800_switch","id":{"systemcode":14,"unitcode":1},"state":"on"}};
-//const ha2 = {"Lamp1":{"name":"Stehlampe","group":"Steckdosen","media":"all"}};
-//                    this.setObjectNotExistsAsync('devices', {
-//                        type: "channel",
-//                        common: {
-//                            name: "devices",
-//                        },
-//                        native: {},
-//                    });
-//this.json2iob.parse('devices', ha1);
-
-
-                            //const items = ['devices', 'rules', 'gui', 'settings', 'hardware', 'registry'];
-                            const items = ['devices'];
-                            for (let i=0; i<items.length; i++) {
-                                if (Object.keys( configval["config"][items[i]] ).length > 0) {
-                                    this.log.info("Create data point " + items[i] + ": " + Object.keys( configval["config"][items[i]] ).length);
-
-                    //this.setObjectNotExistsAsync(items[i], {
-                    //    type: "channel",
-                    //    common: {
-                    //        name: items[i],
-                    //    },
-                    //    native: {},
-                    //});
-
-                    //this.json2iob.parse(items[i], configval["config"][items[i]]);
-                    //this.json2iob.parseduo(items[i], configval["config"][items[i]], configval["config"]["gui"]);
-
-
-                                }
-                            }
-                        } else {
-                            this.log.info("Server not connected!");
-                            this.client.end();
+                const result = data.toString("utf-8").split('\n\n');
+                if (result.length === 1) {
+                    this.jsonString += data;
+                    this.log.debug("Datafirst: " + this.jsonString.toString("utf-8"));
+                } else {
+                    this.jsonString += data;
+                    this.jsonString.split('\n').forEach( async (v) => {
+                        if (v) {
+                            this.log.debug("Data: " + v.trim());
+                            await this.jsonEvaluate(v.trim())
                         }
-	             } else {
-	                 this.log.info("Connect failed!");
-	                 this.client.end();
-	             }
-	         } else {
-	             this.log.info("Can not data found!");
-	             this.client.end();
-	         }
+                    });
+                    this.jsonString = "";
+                }
+
+                if (this.heartbeatInterval) {
+                    clearInterval(this.heartbeatInterval);
+                }
+                this.heartbeatInterval = setInterval(() => {
+                    this.log.debug("Send heartbeat!");
+                    this.client.write('HEART\n');
+                }, 10 * 1000);
             } catch (err) {
                 this.log.error(err);
             }
-
-
-
-
-
-	     //let jsonMessage = data.toString().replace(/\[/g, '').replace(/\]/g, '').replace(/\n/g, '');
-	     //jsonMessage = jsonMessage.replace(/]/g,"");
-            //this.log.info("Data1: " + jsonMessage);
-            //this.log.info("Data2: " + data.toString());
-            //this.log.info("Data3: " + data.toString());
-
-
-	     //const loginval = JSON.parse(jsonMessage.split("}{")[0] + "}");
-	     //const configval = JSON.parse("{" + jsonMessage.split("}{")[1]);
-	     //const configval = "{" + jsonMessage.split("}{")[1];
-	     //this.log.info("Data1: " + loginval.status);
-	     //this.log.info("Data2: " + JSON.stringify(configval["config"]["gui"]));
-
-
-
-            //const jsonMessage = data.toString();
-            //jsonMessage = JSON.parse(jsonMessage);
-            //this.log.info("status: " + jsonMessage.status);
-
-            //this.json2iob.parse("Test", jsonMessage["status"]);
-            //this.json2iob.parse("message", jsonMessage["message"]);
-
-
-
-            
         });
+
         this.client.on('end', () => {
             this.log.info('disconnected from server');
         });
 
         this.client.on('error', (err) => {
-            throw err;
+            this.log.error('Error: ' + err);
         });
     }
 
+    /**
+     * json string evaluate!
+     * @param JSON -> Callback
+     */
+    async jsonEvaluate(JsonStr) {
+        if(JsonStr === "BEAT") {
+            this.log.debug("Heartbeat OK!");
+            return true;
+        }
+
+        try {
+             let jsonMessage = JSON.parse(JsonStr.replace(/\]/g, '').replace(/\[/g, '').replace(/\"on\"/g, '"true"').replace(/\"off\"/g, '"false"'));
+             this.log.debug("Jsonparse: " + JSON.stringify(jsonMessage));
+
+             if(jsonMessage.status === "success" && this.countS === 0) {
+                  this.log.info("Server connected!");
+                  this.client.write('{"action": "request config"}\n');
+                  this.log.info("Send request for the config!");
+                  this.setState("info.connection", true, true);
+                  return true;
+             }
+
+             if(jsonMessage.status === "failure" && this.countS === 0) {
+                  this.log.info("Server not connected!");
+                  this.setState("info.connection", false, true);
+                  return false;
+             }
+
+             if(jsonMessage.status === "success" && this.countS === 1) {
+                  this.log.debug("Command was sent!");
+                  return true;
+             }
+
+             if(jsonMessage.status === "failure" && this.countS === 1) {
+                  this.log.debug("Command was not sent!");
+                  return false;
+             }
+
+             if(jsonMessage.message === "config") {
+                  //const items = ['devices', 'rules', 'gui', 'settings', 'hardware', 'registry'];
+                  const items = ['devices'];
+                  for (let i=0; i<items.length; i++) {
+                       if (Object.keys( jsonMessage["config"][items[i]] ).length > 0) {
+                            this.log.info("Create data point " + items[i] + ": " + Object.keys( jsonMessage["config"][items[i]] ).length);
+                            this.setObjectNotExistsAsync(items[i], {
+                                type: "channel",
+                                common: {
+                                    name: items[i],
+                                },
+                                native: {},
+                            });
+                            if (items[i] === 'devices') {
+                                this.json2iob.parse(jsonMessage["config"]["gui"], items[i], jsonMessage["config"][items[i]]);
+                            } else {
+                                this.json2iob.parse("", items[i], jsonMessage["config"][items[i]]);
+                            }
+                       }
+                  }
+                  return true;
+             }
+
+             if(jsonMessage.action === "send") {
+                  this.countS = 1;
+                  this.log.debug("Send request for the config!");
+                  return true;
+             }
+
+             if(jsonMessage.origin === "update") {
+                  this.log.debug("Receive input!");
+                  Object.keys(jsonMessage).forEach((n) => {
+                  if(n === "values") {
+                       Object.keys(jsonMessage[n]).forEach((v) => {
+                           this.log.debug(this.namespace + ".devices." + jsonMessage["devices"] + "." + v);
+                           this.getForeignObject(this.namespace + ".devices." + jsonMessage["devices"] + "." + v, async (err, obj) => {
+                               if (!obj || !obj.common) {
+                                   this.setObjectNotExists(this.namespace + ".devices." + jsonMessage["devices"] + "." + v, {
+                                       type: 'state',
+                                       common: {
+                                           name: v,
+                                           type: 'number',
+                                           read: true,
+                                           write: false,
+                                           role: 'level',
+                                           def: 0,
+                                           desc: 'Create Timestamp'
+                                       },
+                                       native: {}
+                                   });
+                                   await this.sleep(2000);
+                                   this.setState(this.namespace + ".devices." + jsonMessage["devices"] + "." + v, jsonMessage[n][v], true);
+                               } else {
+                                   this.setState(this.namespace + ".devices." + jsonMessage["devices"] + "." + v, jsonMessage[n][v], true);
+                               }
+                           });
+                       });
+                   }
+               });
+               return true;
+             }
+
+        } catch (err) {
+            this.log.error(err);
+            return false;
+        }
+    }
+
+    /**
+     * Sleep!
+     * 
+     */
     sleep(ms) {
         return new Promise((resolve) => setTimeout(resolve, ms));
     }
@@ -216,12 +234,39 @@ this.log.info("True NOK: " + this.checkip);
      * @param {ioBroker.State | null | undefined} state
      */
     onStateChange(id, state) {
-        if (state) {
-            // The state was changed
-            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-        } else {
-            // The state was deleted
-            this.log.info(`state ${id} deleted`);
+        if (state && !state.ack) {
+            this.log.debug('Send id: ' + id);
+            this.log.debug('Send state: ' + state.val);
+            let states = null;
+            const lastsplit = id.split('.')[id.split('.').length-1];
+            const name = id.split('.')[id.split('.').length-2];
+            this.log.debug('Send last: ' + lastsplit);
+            if(lastsplit === "state") {
+                states = (state.val) ? "on" : "off";
+                this.client.write(JSON.stringify({
+                    "action": "control",
+                    "code": {
+                        "device": name,
+                        "state": states
+                    }
+                }));
+            } else if(lastsplit === "dimlevel") {
+                states = (state.val === 0) ? "off" : "on";
+                this.client.write(JSON.stringify({
+                    "action": "control",
+                    "code": {
+                        "device": name,
+                        "state": states,
+                        "values": {
+                            "dimlevel": state.val
+                        }
+                    }
+                }));
+            } else {
+                this.log.info("Command " + lastsplit + " not implemented");
+                this.log.info("Path: " + lastsplit);
+                this.log.info("Value: " + lastsplit);
+            }
         }
     }
 }
@@ -231,8 +276,8 @@ if (require.main !== module) {
     /**
      * @param {Partial<utils.AdapterOptions>} [options={}]
      */
-    module.exports = (options) => new PilightAPI(options);
+    module.exports = (options) => new Pilightapi(options);
 } else {
     // otherwise start the instance directly
-    new PilightAPI();
+    new Pilightapi();
 }
